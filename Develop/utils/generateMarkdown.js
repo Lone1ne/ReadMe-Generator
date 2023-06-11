76f795745ea14b892070ef78f64aa7af3201cc44
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-06-11 19:58:22 -0400

    Updated renderLicenseSection to call renderLiscenseLink, updated renderLicenseLink to return license section and link to the license, created licenseLink object to store urls for the licenses.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,6 +16,16 @@ const licenseBadges = {
     "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
   "No license": "",
 };
+const licenseLink = {
+  Apache: "(https://opensource.org/licenses/Apache-2.0)",
+  Boost: "(https://www.boost.org/LICENSE_1_0.txt)",
+  BSD3: "(https://opensource.org/licenses/BSD-3-Clause)",
+  Eclipse: "(https://opensource.org/licenses/EPL-1.0)",
+  GPLv3: "(https://www.gnu.org/licenses/gpl-3.0)",
+  MIT: "(https://opensource.org/licenses/MIT)",
+  Unlicense: "(http://unlicense.org/)",
+  "No license": "",
+};
 
 function renderLicenseBadge(license) {
   return licenseBadges[license] || "";
@@ -23,7 +33,14 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "No license") {
+    return "";
+  } else {
+    return `## License
+  This project is licensed under the ${license} license. Link to license: ${licenseLink[license]}`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -31,8 +48,7 @@ function renderLicenseSection(license) {
   if (license === "No license") {
     return "";
   } else {
-    return `## License
-  This project is licensed under the ${license} license.`;
+    return renderLicenseLink(license);
   }
 }
 
